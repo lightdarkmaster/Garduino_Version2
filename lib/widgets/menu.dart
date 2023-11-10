0432b3f14a220d4ccb5a57ef3525ae3c0324c0dd
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-11-11 07:45:11 +0800

    Add Setting page

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -7,6 +7,7 @@ import 'package:garduino_dashboard/main.dart';
 import 'package:garduino_dashboard/model/menu_modal.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:garduino_dashboard/widgets/dashboard_page.dart';
+import 'package:garduino_dashboard/widgets/settings_page.dart';
 
 import 'controller_page.dart';
 import 'aboutUs.dart';
@@ -103,7 +104,7 @@ class _MenuState extends State<Menu> {
                       case 4: //Settings
                         Navigator.of(context).push(
                           MaterialPageRoute(
-                              builder: (context) => DashboardPage()),
+                              builder: (context) => const SettingsPage2()),
                         );
                         break;
                       case 5: //About Us
